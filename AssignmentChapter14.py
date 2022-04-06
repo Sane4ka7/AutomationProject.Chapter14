e4a99937219678f16d7bfcdbb96cceba221bef62
github_Sane4ka7 <sanya.olya.s@gmail.com>
2022-04-05 22:26:00 -0400

    AssignmentChapter14.py got added

---
+++ AssignmentChapter14.py
@@ -0,0 +1,10 @@
+print ("_____")
+print_this_line = "|   /"
+print (print_this_line)
+print_this_line = "|  /"
+print (print_this_line)
+print_this_line = "| /"
+print (print_this_line)
+print_this_line = "|/"
+print (print_this_line)
+
